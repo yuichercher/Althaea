b31749ada0305c419c5f61871de7e127ffad6a96
CherCher <yuichercher@gmail.com>
2025-08-04 23:23:28 +0800

    Initial Commit

---
+++ Althaea/AlthaeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlthaeaApp.swift
+//  Althaea
+//
+//  Created by Yui Cher on 2025/8/4.
+//
+
+import SwiftUI
+
+@main
+struct AlthaeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Althaea/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Althaea
+//
+//  Created by Yui Cher on 2025/8/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
